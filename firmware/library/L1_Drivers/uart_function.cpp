27aa97b99e24c34f27fda7ffc1e26ba55325a4ca
jeremy <chaujeremy7@gmail.com>
2018-08-02 22:01:26 -0700

    Added a dummy uart functioncode file for board testing.

---
+++ firmware/library/L1_Drivers/uart_function.cpp
@@ -0,0 +1,27 @@
+
+#include "L1_Drivers/uart.hpp"
+
+int main()
+{
+    char * receive;
+    Uart object;
+    
+    // Set to use UART 2
+    // Look at what pins are gonna be used
+    if(object.Initialize(9600, 2) == 0)
+    {
+    	printf("Fail!");
+    }
+    
+    char sending = "Hello World";
+    object.Send(sending, 20);
+
+    if(!object.Receive(receive, 30))
+    {
+    	printf("Fail!");
+    }
+    else
+    	printf(receive);
+
+    return 0;
+}
